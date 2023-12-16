2a32e662ba028eb8700068c4f1d338f07bdd19e5
Karani Wachira <wachirakeith@gmail.com>
2023-12-16 10:03:13 +0300

    feat: created navbar, skills, blog, skill & projects

---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: " ",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: " ",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: " ",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: " ",
+  },
+];
+
+export const testimonials = [
+  {
+    quote: " In this article, I will take you through the step-by-step walkthrough of Pushing your R project to GitHub from RStudio.",
+    image: "https://cdn.hashnode.com/res/hashnode/image/upload/v1682787565658/6dc3bc55-8be0-4447-895f-5f1cadb17331.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    name: "RStudio to GitHub: Version Control Made Easy ",
+    company: "hashnode",
+  },
+  {
+    quote:
+      "What design features can you derive from childhood toy cars that satisfied your experience when playing with it",
+    image: "https://cdn.hashnode.com/res/hashnode/image/upload/v1695270363144/bab669c1-6107-4e8f-9393-4ce41eb7cdfc.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    name: "Toy Cars and User Experience Design",
+    company: "hashnode",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "ML with R",
+  "UI/UX",
+  "Nodejs",
+  "Azure",
+];
